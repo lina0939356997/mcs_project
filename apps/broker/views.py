b876609ff052ad98487aa499b307c641f7af4990
Zscing <71181894+lina0939356997@users.noreply.github.com>
2022-04-13 16:30:56 +0800

    update

--- apps/broker/views.py
+++ apps/broker/views.py
@@ -1,7 +1,7 @@
 import config
 import datetime
 from ..models import PosViewModel
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_paginate import Pagination, get_page_parameter
 from .models import CommModel, CommLineModel
 from apps.decorators import login_required
@@ -11,17 +11,16 @@ from utils import restful
 
 bp = Blueprint("broker", __name__, url_prefix='/broker')
 
-
 @bp.route('/')
 @login_required
-def show_comm():
+def show_comms():
     result = db.session.query(
         PosViewModel.order_num,
         PosViewModel.group_name,
         PosViewModel.car,
         PosViewModel.order_date,
         func.sum(PosViewModel.amt).label('subtotal')
-    ).filter(PosViewModel.sale_line_id.notin_(CommLineModel.sale_line_id)) \
+    ).filter(PosViewModel.sale_line_id.isnot(None)) \
         .group_by(PosViewModel.order_num,
                   PosViewModel.group_name,
                   PosViewModel.car,
@@ -29,14 +28,18 @@ def show_comm():
         .all()
 
     context = {
-        'commission': result,
+        'commissions': result,
     }
 
     return render_template('broker/brokers.html', **context)
 
 
-
-
+@bp.route('/count/', methods=['POST'])
+def count():
+    form_commissions = request.form.getlist("commission")
+    for form_commission in form_commissions:
+        print(form_commission)
+    return "success"
 
 
 
