e76e9cc244f6838ec4461ee6a2424f8fa5722999
huangweizhong <yo577306@gmail.com>
2022-04-13 17:17:41 +0800

    將apps.models中的broker, comm_broker的model換到broker/models中

--- apps/broker/models.py
+++ apps/broker/models.py
@@ -42,3 +42,36 @@ class CommLineModel(db.Model):
 
     creator = db.relationship("MCUser", foreign_keys=[created_by], backref='ccommlines')
     updater = db.relationship("MCUser", foreign_keys=[updated_by], backref='ucommlines')
+
+
+class BrokerModel(db.Model):
+    __tablename__ = 'broker'
+    broker_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    broker_name = db.Column(db.String(50), nullable=False)
+    broker_num = db.Column(db.String(10), nullable=False)
+    travel = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.String(10), nullable=False)
+    address = db.Column(db.String(200), nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False)
+    created_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+    updated_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
+
+    creator = db.relationship("MCUser", foreign_keys=[created_by], backref='cbrokers')
+    updater = db.relationship("MCUser", foreign_keys=[updated_by], backref='ubrokers')
+
+
+class CommBrokerModel(db.Model):
+    __tablename__ = 'comm_broker'
+    comm_broker_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    broker_id = db.Column(db.Integer, db.ForeignKey("broker.broker_id"), nullable=False)
+    comm_id = db.Column(db.Integer, db.ForeignKey("comm.comm_id"), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    created_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+    updated_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
+
+    broker = db.relationship("BrokerModel", foreign_keys=[broker_id], backref='claimants')
+    comm = db.relationship("CommModel", foreign_key=[comm_id], backref='comms')
+    creator = db.relationship("MCUser", foreign_keys=[created_by], backref='ccommbrokers')
+    updater = db.relationship("MCUser", foreign_keys=[updated_by], backref='ucommbrokers')
