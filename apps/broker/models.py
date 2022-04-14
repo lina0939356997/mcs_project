2b491c903dcee99bcebcca9b550e12b73d3f3e55
huangweizhong <yo577306@gmail.com>
2022-04-14 15:17:06 +0800

    PayModel

--- apps/broker/models.py
+++ apps/broker/models.py
@@ -15,9 +15,9 @@ class CommModel(db.Model):
     comm_type = db.Column(db.String(30), nullable=False)
     cal_type = db.Column(db.String(30), nullable=False)
     status = db.Column(db.String(20), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     created_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     updated_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
 
     creator = db.relationship("UserModel", foreign_keys=[created_by], backref='ccomms')
@@ -35,9 +35,9 @@ class CommLineModel(db.Model):
     item_no = db.Column(db.String(50), nullable=False)
     qty = db.Column(db.Integer, nullable=False, default=0)
     amt = db.Column(db.Integer, nullable=False, default=0)
-    created_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     created_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     updated_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
 
     creator = db.relationship("UserModel", foreign_keys=[created_by], backref='ccommlines')
@@ -52,9 +52,9 @@ class BrokerModel(db.Model):
     travel = db.Column(db.String(50), nullable=False)
     phone = db.Column(db.String(10), nullable=False)
     address = db.Column(db.String(200), nullable=True)
-    created_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     created_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     updated_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
 
     creator = db.relationship("UserModel", foreign_keys=[created_by], backref='cbrokers')
@@ -66,12 +66,34 @@ class CommBrokerModel(db.Model):
     comm_broker_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     broker_id = db.Column(db.Integer, db.ForeignKey("broker.broker_id"), nullable=False)
     comm_id = db.Column(db.Integer, db.ForeignKey("comm.comm_id"), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     created_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     updated_by = db.Column(db.String(10), db.ForeignKey("mc_user_all.account"), nullable=False)
 
     broker = db.relationship("BrokerModel", foreign_keys=[broker_id], backref='claimants')
     comm = db.relationship("CommModel", foreign_keys=[comm_id], backref='comms')
     creator = db.relationship("UserModel", foreign_keys=[created_by], backref='ccommbrokers')
     updater = db.relationship("UserModel", foreign_keys=[updated_by], backref='ucommbrokers')
+
+
+class PayModel(db.Model):
+    __tablename__ = 'pay'
+    pay_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pay_date = db.Column(db.DateTime, nullable=False)
+    decl_type = db.Column(db.String(20), nullable=False)
+    broker_id = db.Column(db.Integer, db.ForeignKey("broker.broker_id"), nullable=True)
+    invoice_num = db.Column(db.String(10), nullable=True)
+    uniform_num = db.Column(db.String(8), nullable=True)
+    site_name = db.Column(db.String(50), nullable=True)
+    pay_status = db.Column(db.String(20), nullable=False, default='NEW')  # NEW/CLOSED/REVERSE
+    decl_status = db.Column(db.String(1), nullable=False, default='N')  # Y/N
+    total_amt = db.Column(db.Integer, nullable=False)
+    tax_amt = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    created_by = db.Column(db.String(10), db.ForeignKey("user.account"), nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    updated_by = db.Column(db.String(10), db.ForeignKey("user.account"), nullable=False)
+
+    creator = db.relationship("UserModel", foreign_keys=[created_by], backref='cpays')
+    updater = db.relationship("UserModel", foreign_keys=[updated_by], backref='upays')
