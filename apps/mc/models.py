80bc63a675827ed48c2a276983689189b8145c6c
huangweizhong <yo577306@gmail.com>
2022-04-14 15:03:19 +0800

    將建立及更新時間給予預設值

--- apps/mc/models.py
+++ apps/mc/models.py
@@ -9,9 +9,9 @@ class UserModel(db.Model):
     password = db.Column(db.String(10), nullable=False)
     name = db.Column(db.String(20), nullable=False)
     permission = db.Column(db.String(10), nullable=False)  # admin-管理者, user-一般使用者
-    created_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     created_by = db.Column(db.String(10), db.ForeignKey("user.account"), nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     updated_by = db.Column(db.String(10), db.ForeignKey("user.account"), nullable=False)
 
     creator = db.relationship("UserModel", foreign_keys=[created_by], backref='cusers')
