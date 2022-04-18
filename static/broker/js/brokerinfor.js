7ae7511e9bc190edbc53d8204e233413ec20635c
Zscing <71181894+lina0939356997@users.noreply.github.com>
2022-04-18 11:47:01 +0800

    UPDATE

--- static/broker/js/brokerinfor.js
+++ static/broker/js/brokerinfor.js
@@ -3,43 +3,43 @@ $(function () {
         event.preventDefault();
         var self = $(this);
         var dialog = $("#brokerinfor-dialog");
-        var borker_nameInput = $("input[name='borker_name']");
-        var borker_numInput = $("input[name='borker_num']");
+        var broker_nameInput = $("input[name='broker_name']");
+        var broker_numInput = $("input[name='broker_num']");
         var travelInput = $("input[name='travel']");
         var phoneInput = $("input[name='phone']");
         var addressInput = $("input[name='address']");
 
 
-        var borker_name = borker_nameInput.val();
-        var borker_num = borker_numInput.val();
+        var broker_name = broker_nameInput.val();
+        var broker_num = broker_numInput.val();
         var travel = travelInput.val();
         var phone = phoneInput.val();
         var address = addressInput.val();
         var submitType = self.attr('data-type');
-        var borker_id = self.attr("data-borker_id");
+        var broker_id = self.attr("data-broker_id");
 
 
-        if(!borker_name || !borker_num || !travel || !phone){
+        if(!broker_name || !broker_num || !travel || !phone){
             zlalert.alertInfoToast('請輸入完整資訊！');
             return;
         }
 
         var url = '';
         if(submitType == 'update'){
-            url = '/ubrokerinfor/';
+            url = '/broker/ubrokerinfor/';
         }else{
-            url = '/abrokerinfor/';
+            url = '/broker/abrokerinfor/';
         }
 
         zlajax.post({
             "url": url,
             'data':{
-                'borker_name':borker_name,
-                'borker_num': borker_num,
+                'broker_name':broker_name,
+                'broker_num': broker_num,
                 'travel': travel,
                 'phone': phone,
                 'address':address,
-                'borker_id':borker_id
+                'broker_id':broker_id
             },
             'success': function (data) {
                 dialog.modal("hide");
@@ -62,27 +62,27 @@ $(function () {
         var self = $(this);
         var dialog = $("#brokerinfor-dialog");
         var tr = self.parent().parent();
-        var borker_name = tr.attr("data-borker_name");
-        var borker_num = tr.attr("data-borker_num");
+        var broker_name = tr.attr("data-broker_name");
+        var broker_num = tr.attr("data-broker_num");
         var travel = tr.attr("data-travel");
         var phone = tr.attr("data-phone");
         var address = tr.attr("data-address");
         dialog.modal('show')
 
-        var borker_nameInput = dialog.find("input[name='borker_name']");
-        var borker_numInput = dialog.find("input[name='borker_num']");
+        var broker_nameInput = dialog.find("input[name='broker_name']");
+        var broker_numInput = dialog.find("input[name='broker_num']");
         var travelInput = dialog.find("input[name='travel']");
         var phoneInput = dialog.find("input[name='phone']");
         var addressInput = dialog.find("input[name='address']");
         var saveBtn = dialog.find("#save-brokerinfor-btn");
 
-        borker_nameInput.val(borker_name);
-        borker_numInput.val(borker_num);
+        broker_nameInput.val(broker_name);
+        broker_numInput.val(broker_num);
         travelInput.val(travel);
         phoneInput.val(phone);
         addressInput.val(address);
         saveBtn.attr("data-type",'update');
-        saveBtn.attr('data-borker_id',tr.attr('data-borker_id'));
+        saveBtn.attr('data-broker_id',tr.attr('data-broker_id'));
     });
 });
 
@@ -90,14 +90,14 @@ $(function () {
     $(".delete-brokerinfor-btn").click(function (event) {
         var self = $(this);
         var tr = self.parent().parent();
-        var borker_id = tr.attr('data-borker_id');
+        var broker_id = tr.attr('data-broker_id');
         zlalert.alertConfirm({
             "msg":"確認刪除？",
             'confirmCallback': function () {
                 zlajax.post({
-                    'url': '/dbrokerinfor/',
+                    'url': '/broker/dbrokerinfor/',
                     'data':{
-                        'borker_id': borker_id
+                        'broker_id': broker_id
                     },
                     'success': function (data) {
                         if(data['code'] === 200){
